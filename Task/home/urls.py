e672dda77b856bac06717b0f6ccc0c5dd8e2cbc8
insiyazain <insiyazain123@gmail.com>
2024-12-25 08:55:54 +0500

    calling data from database

--- Task/home/urls.py
+++ Task/home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('book/<int:book_id>/',views.book_detail)
 ]
 
--- Task/home/views.py
+++ Task/home/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Book,Loan,Member
 
 # Create your views here.
 
 def index(request):
-    return render(request,"home/index.html")
+    books=Book.objects.all()
+    return render(request,"home/index.html", {'books': books})
+
+def book_detail(request,book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'home/book_detail.html', {'book': book})
+
